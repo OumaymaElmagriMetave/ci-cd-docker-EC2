de376b7120cf1511d8ec69f5911e206f53e39733
OumaymaElmagriMetave <oumayma.elmaghri@metave.us>
2023-11-13 16:19:34 +0100

    test ouma

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Docker image in <code>EC2</code> using ci cd.
         </p>
         <a
           className="App-link"
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          React app by ouma
         </a>
       </header>
     </div>
